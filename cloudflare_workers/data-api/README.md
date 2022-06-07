901f45dba9897e86bf99bc72f5753be51586aa82
Jagadesh Munta <jagadesh.munta@couchbase.com>
2022-06-07 13:40:54 -0700

    Couchbase data api using cloudflare sample

---
+++ cloudflare_workers/data-api/README.md
@@ -0,0 +1,19 @@
+# Couchbase data api
+See the [https://dapi.couchbase.live ](https://dapi.couchbase.live/v1/spec)
+
+## Access data using cloudflare app
+
+https://data-api.couchbase-live.workers.dev/&limit=100
+
+To change the bucket:
+&bucketName=beer-sample
+
+
+
+Steps to deploy:
+
+wrangler dev --> to locally run
+
+To publish at https://data-api.couchbase-live.workers.dev
+wrangler login 
+wrangler publish
---
+++ cloudflare_workers/data-api/index.js
@@ -0,0 +1,128 @@
+/**
+ * Example someHost is set up to take in a JSON request
+ * Replace url with the host you wish to send requests to
+ * @param {string} someHost the host to send the request to
+ * @param {string} url the URL to send the request to
+ */
+ const apiHost = 'https://dapi.couchbase.live';
+ const apiVersion = '/v1';
+ //const url = apiHost + apiVersion+ '/scopes/_default/collections/_default/docs';
+ 
+ /**
+  * gatherResponse awaits and returns a response body as a string.
+  * Use await gatherResponse(..) in an async function to get the response body
+  * @param {Response} response
+  */
+ async function gatherResponse(response) {
+   const { headers } = response;
+   const contentType = headers.get('content-type') || '';
+   if (contentType.includes('application/json')) {
+      //return JSON.stringify(await response.json());
+      text = await response.text();
+      //console.log(text);
+      let documents = JSON.parse(text);
+      documents = documents['docs'];
+      //console.log(documents);
+      let results = '<table border="true"><tr><th>#</th>';
+        
+      if (documents.length > 0) {
+        let keys = Object.keys(documents[0]);
+        for (let i = 0; i < keys.length; i++) {
+            results += '<th>'+ keys[i] +'</th>';
+        }
+        results += '</tr>';
+        for (d = 1; d <= documents.length; d++) {
+          results += '<tr><td>'+ d +'</td>';
+          for (let i = 0; i < keys.length; i++) {
+            val = documents[d-1][keys[i]];
+            if (val == null || val == undefined) {
+              val = '';
+            } else {
+              val = JSON.stringify(val);
+            }
+            results += '<td>'+val+'</td>';
+          }
+          results += '</tr>';
+        }
+      } else {
+        results += '<td>No documents found</td><td>Check bucket='+
+              ', scope=, collection= query parameters</td></tr>';
+      }
+      results += '</table>';
+      return results;
+      
+   } else if (contentType.includes('application/text')) {
+     return response.text();
+   } else if (contentType.includes('text/html')) {
+     return response.text();
+   } else {
+     console.log('Unknown content-type: ' + contentType);
+     return response.text();
+     
+   }
+ }
+ 
+ async function handleRequest(request) {
+   const { searchParams } = new URL(request.url);
+   bucket = searchParams.get('bucket') || searchParams.get('database') ||  searchParams.get('data') || 'travel-sample';
+   scope = searchParams.get('scope') || searchParams.get('tenant') || '_default';
+   collection = searchParams.get('collection') || searchParams.get('table') || '_default';
+
+   url = apiHost + apiVersion +'/scopes/'+scope+'/collections/'+collection+'/docs';
+    queryParams = '?';
+    let token = searchParams.get('token');
+    /*if (token != null && token != '') {
+      token = token.replaceAll(' ', '+');
+    } else {
+      token = 'CTEH1_a39ec8fd216d75ee40c05989c6b1d946149414398517ef1206c8046bc7a4a715_B6eyhenCiAd0cKCKfLsXifiiPbifviVCyS+CHdTm2tYfuhE6DDVtwsFj2wgR9AowKHcRmJYN7ghQcPyRuvPBPA13Q4Moz/zWqfYDcW4HI0DClL4Ddk73iCvXuWG9TSaG4sCoFc88XEPZ6uXgKRc8RBLx0ObTtuAy98OcxJnH9IgzGFvXIT3KkbLmk+z9RXK8fcBgT5Nwjwj2UcGV03U2whThFKhhkEERrZJz4pDVeCqD8MEfMQLqAlIxqBnsbK/DS95U2pKKu2ghgic=';
+    }*/
+    for (let p of searchParams.keys()) {
+      queryParams += p +'=' + searchParams.get(p)+'&';
+    }
+    let url2 = url + queryParams;
+    
+    query = searchParams.get('query');
+    if (query != null && query != '') {
+      querydata = {"query": query};
+    }
+
+    
+    console.log("final url="+url2);
+    
+    const init = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //body: JSON.stringify(querydata),
+    };
+    
+   const response = await fetch(url2, init);
+   const results = await gatherResponse(response);
+   //return new Response(results, init);
+
+   let html_content = '';
+    let html_style = 'body{padding:1em; font-family: sans-serif;} h1{color:#f6821f;}';
+
+    html_content += '<p> '+ results+'</p>';
+
+    let html = `<!DOCTYPE html>
+  <head>
+    <title> Couchbase: ${bucket}.${scope}.${collection} </title>
+    <style> ${html_style} </style>
+  </head>
+  <body>
+    <h1>Couchbase data from: ${bucket}.${scope}.${collection}</h1>
+    ${html_content}
+  </body>`;
+
+    return new Response(html, {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    });
+
+ }
+ 
+ addEventListener('fetch', event => {
+   return event.respondWith(handleRequest(event.request));
+ });
