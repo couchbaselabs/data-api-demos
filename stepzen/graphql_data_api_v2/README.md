2db916cdcc842d22989aaf92822443a2ee3aed82
Jagadesh Munta <jagadesh.munta@couchbase.com>
2022-07-12 04:47:35 -0700

    Another version based on the labest couchbase data api changes

---
+++ stepzen/graphql_data_api_v2/README.md
@@ -0,0 +1,8 @@
+# Couchbase Data API Demo using stepzen graphql
+
+Steps:
+--
+* Run 
+  * stepzen start
+
+* Access proxy URL from browser: http://localhost:5001/api/data-api-graphql-couchbase-live
---
+++ stepzen/graphql_data_api_v2/stepzen_run.sh
@@ -0,0 +1,4 @@
+curl https://shiyan.stepzen.net/api/data-api-demo-graphql-couchbase-live/__graphql \
+   --header "Authorization: Apikey shiyan::stepzen.net+1000::536307ca7a968a269065e67f65da9850a2b1362071a74daf5aeeff184f621b5c" \
+   --header "Content-Type: application/json" \
+   --data '{"query": "{ getAllTravelDocuments { name type} }"}'
